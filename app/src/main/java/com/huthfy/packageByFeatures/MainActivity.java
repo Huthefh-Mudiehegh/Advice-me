8d53ce38747915b6c017c20bec70ad3364d71947
Huthefh <huthefh2019@gmail.com>
2022-05-22 00:50:08 +0300

    create the project

---
+++ app/src/main/java/com/huthfy/packageByFeatures/MainActivity.java
@@ -0,0 +1,16 @@
+package com.huthfy.packageByFeatures;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.huthfy.packageByLayer.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
